6936f4f2633738aab8d910973e9ce0312781d3a9
Vadim Moroz <v9858480345@yandex.ru>
2021-11-24 15:31:31 +0300

    avtorized uzer and localStorege

--- js/main.js
+++ js/main.js
@@ -8,3 +8,73 @@ close.addEventListener("click", toggleModal);
 function toggleModal() {
   modal.classList.toggle("is-open");
 }
+
+// 1 day 
+
+const buttonAuth = document.querySelector('.button-auth')
+const modalAuth = document.querySelector('.modal-auth')
+const closeAuth = document.querySelector('.close-auth')
+const logInForm = document.querySelector('#logInForm') 
+const loginInput = document.querySelector('#login')
+const uzerName = document.querySelector('.user-name')
+const buttonOut = document.querySelector('.button-out')
+
+let login = localStorage.getItem('key')
+
+buttonAuth.addEventListener("click", toggleModalAuth)
+closeAuth.addEventListener("click", toggleModalAuth)
+
+function toggleModalAuth() {
+  modalAuth.classList.toggle("is-open")
+}
+
+function authorized() {
+  function logOut() {
+    login = null
+    localStorage.removeItem('login')
+    buttonAuth.style.display = ''
+    uzerName.style.display = ''
+    buttonOut.style.display = ''
+    buttonOut.removeEventListener("click", logOut)
+    checkAuth()
+  }
+  console.log('Пользователь авторизован')
+  uzerName.textContent = login
+  buttonAuth.style.display = 'none'
+  uzerName.style.display = 'inline'
+  buttonOut.style.display = 'block'
+
+  buttonOut.addEventListener("click", logOut)
+}
+
+function  notAuthorized() {
+  console.log('Пользователь не авторизован')
+
+  function logIn(e) {
+    e.preventDefault()
+    login = loginInput.value
+
+    localStorage.setItem('key', login)
+
+    toggleModalAuth()
+    logInForm.removeEventListener('submit', logIn)
+    buttonAuth.removeEventListener("click", toggleModalAuth)
+    closeAuth.removeEventListener("click", toggleModalAuth)
+    logInForm.reset()
+    checkAuth()
+  }
+
+  logInForm.addEventListener('submit', logIn)
+  buttonAuth.addEventListener("click", toggleModalAuth)
+  closeAuth.addEventListener("click", toggleModalAuth)
+}
+
+function checkAuth() {
+  if(login){
+    authorized()
+  }else{
+    notAuthorized()
+  
+  }
+}
+checkAuth()
